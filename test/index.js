e9c92a537c12edb7bf54f1416cfe06eb2b7d70b0
Rui Quelhas <rfpquelhas@gmail.com>
2017-11-12 16:37:54 +0000

    Update module to support hapi v17 API

--- test/index.js
+++ test/index.js
@@ -10,29 +10,22 @@ const Form = require('multi-part');
 const Hapi = require('hapi');
 const Lab = require('lab');
 
-const Coutts = require('../lib/');
+const Coutts = require('../');
 
 const lab = exports.lab = Lab.script();
 
 lab.experiment('coutts', () => {
 
+    let png;
     let server;
 
-    lab.before((done) => {
+    lab.before(async () => {
 
         server = new Hapi.Server();
-        server.connection();
-
-        const plugin = {
-            register: Coutts,
-            options: {
-                whitelist: ['image/png']
-            }
-        };
 
         const main = {
-            config: {
-                handler: (request, reply) => reply(request.payload),
+            options: {
+                handler: (request) => request.payload,
                 payload: {
                     output: 'file',
                     parse: false
@@ -43,8 +36,8 @@ lab.experiment('coutts', () => {
         };
 
         const ignore = {
-            config: {
-                handler: (request, reply) => reply(),
+            options: {
+                handler: () => null,
                 payload: {
                     output: 'data',
                     parse: true
@@ -54,78 +47,96 @@ lab.experiment('coutts', () => {
             path: '/ignore'
         };
 
-        server.register(plugin, (err) => {
+        server.route([main, ignore]);
 
-            if (err) {
-                return done(err);
+        await server.register( {
+            plugin: Coutts,
+            options: {
+                whitelist: ['image/png']
             }
-
-            server.route([main, ignore]);
-            done();
         });
     });
 
-    lab.test('should return control to the server if the route parses or does not handle stream request payloads', (done) => {
+    lab.beforeEach(() => {
+        // Create fake png file
+        png = Path.join(Os.tmpdir(), 'foo.png');
 
-        server.inject({ method: 'POST', payload: {}, url: '/ignore' }, (response) => {
+        return new Promise((resolve, reject) => {
 
-            Code.expect(response.statusCode).to.equal(200);
-            Code.expect(response.headers['content-validation']).to.equal('success');
-            Code.expect(response.headers['content-type']).to.not.exist();
-            done();
+            Fs.createWriteStream(png)
+                .on('error', reject)
+                .end(Buffer.from('89504e470d0a1a0a', 'hex'), resolve);
         });
     });
 
-    lab.test('should return control to the server if the payload does not contain any file', (done) => {
+    lab.test('should return control to the server if the route parses or does not handle stream request payloads', async () => {
+
+        const { headers, statusCode } = await server.inject({
+            method: 'POST',
+            payload: {},
+            url: '/ignore'
+        });
+
+        Code.expect(statusCode).to.equal(200);
+        Code.expect(headers['content-validation']).to.equal('success');
+        Code.expect(headers['content-type']).to.not.exist();
+    });
+
+    lab.test('should return control to the server if the payload does not contain any file', async () => {
 
         const form = new Form();
         form.append('foo', 'bar');
 
-        server.inject({ headers: form.getHeaders(), method: 'POST', payload: form.stream(), url: '/main' }, (response) => {
-
-            Code.expect(response.statusCode).to.equal(200);
-            Code.expect(response.headers['content-validation']).to.equal('success');
-            Code.expect(Content.type(response.headers['content-type']).mime).to.equal('application/json');
-            Code.expect(response.result).to.include(['path', 'bytes']);
-            done();
+        const { headers, result, statusCode } = await server.inject({
+            headers: form.getHeaders(),
+            method: 'POST',
+            payload: form.stream(),
+            url: '/main'
         });
+
+        Code.expect(statusCode).to.equal(200);
+        Code.expect(headers['content-validation']).to.equal('success');
+        Code.expect(Content.type(headers['content-type']).mime).to.equal('application/json');
+        Code.expect(result).to.include(['path', 'bytes']);
     });
 
-    lab.test('should return error if the payload cannot be parsed', (done) => {
-
-        const png = Path.join(Os.tmpdir(), 'foo.png');
-        Fs.createWriteStream(png).end(Buffer.from('89504e47', 'hex'));
+    lab.test('should return error if the payload cannot be parsed', async () => {
 
         const form = new Form();
         form.append('file', Fs.createReadStream(png));
 
-        server.inject({ headers: { 'Content-Type': 'application/json' }, method: 'POST', payload: form.stream(), url: '/main' }, (response) => {
-
-            Code.expect(response.statusCode).to.equal(415);
-            Code.expect(response.headers['content-validation']).to.not.exist();
-            Code.expect(Content.type(response.headers['content-type']).mime).to.equal('application/json');
-            Code.expect(response.result).to.not.include(['path', 'bytes']);
-            done();
+        const { headers, result, statusCode } = await server.inject({
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            payload: form.stream(),
+            url: '/main'
         });
+
+        Code.expect(statusCode).to.equal(415);
+        Code.expect(headers['content-validation']).to.not.exist();
+        Code.expect(Content.type(headers['content-type']).mime).to.equal('application/json');
+        Code.expect(result).to.not.include(['path', 'bytes']);
     });
 
-    lab.test('should return control to the server if all files the in payload are allowed', (done) => {
-
-        const png = Path.join(Os.tmpdir(), 'foo.png');
-        Fs.createWriteStream(png).end(Buffer.from('89504e47', 'hex'));
+    lab.test('should return control to the server if all files in the payload are allowed', async () => {
 
         const form = new Form();
         form.append('file1', Fs.createReadStream(png));
         form.append('file2', Fs.createReadStream(png));
         form.append('foo', 'bar');
 
-        server.inject({ headers: form.getHeaders(), method: 'POST', payload: form.stream(), url: '/main' }, (response) => {
-
-            Code.expect(response.statusCode).to.equal(200);
-            Code.expect(response.headers['content-validation']).to.equal('success');
-            Code.expect(Content.type(response.headers['content-type']).mime).to.equal('application/json');
-            Code.expect(response.result).to.include(['path', 'bytes']);
-            done();
+        const { headers, result, statusCode } = await server.inject({
+            headers: form.getHeaders(),
+            method: 'POST',
+            payload: form.stream(),
+            url: '/main'
         });
+
+        Code.expect(statusCode).to.equal(200);
+        Code.expect(headers['content-validation']).to.equal('success');
+        Code.expect(Content.type(headers['content-type']).mime).to.equal('application/json');
+        Code.expect(result).to.include(['path', 'bytes']);
     });
 });
